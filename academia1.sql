55fbe2c3bffb0ac554aff18c855e2e57df383179
VitoriaSoaresd <112016371+VitoriaSoaresd@users.noreply.github.com>
2022-12-12 16:43:14 -0300

    sql update

--- academia1.sql
+++ academia1.sql
@@ -14,6 +14,26 @@ CREATE TABLE addres (
 	state VARCHAR (255) NOT NULL
 );
 
+INSERT INTO addres (
+    zipcode,
+    street,
+    district,
+    city,
+    state
+) VALUES (
+    '01020-345',
+    'Rua Gangorra',
+    'Verdinho',
+    'Rio de Janeiro',
+    'RJ'
+), (
+    '12345-678',
+    'Rua Girassol',
+    'Fazenda',
+    'Rio de Janeiro',
+    'RJ'
+);
+
 CREATE TABLE student (
     sid INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR (255) NOT NULL,
@@ -28,6 +48,41 @@ CREATE TABLE student (
     photo VARCHAR (255),
 
     FOREIGN KEY (zipcode) REFERENCES addres (zipcode)
+);
+
+INSERT INTO student (
+    sid, 
+    name, 
+    email, 
+    cellphone, 
+    document, 
+    birth, 
+    zipcode, 
+    housenumber,
+    complement,
+    photo
+) VALUES (
+    '1',
+    'Nina Norevalga',
+    'nina@nininha.com',
+    '(21) 123456789',
+    '2299',
+    '1997-07-14',
+    '01020-345',
+    '03',
+    'apto 144',
+    'https://randomuser.me/api/portraits/women/17.jpg'
+), (
+    '2',
+    'Lula Molusco',
+    'fenda@dobiquine.com',
+    '(21) 987654321',
+    '7259',
+    '2001-01-29',
+    '12345-678',
+    '1476',
+    'fundos',
+    'https://randomuser.me/api/portraits/men/3.jpg'
 );
 
 CREATE TABLE employee (
@@ -46,6 +101,52 @@ CREATE TABLE employee (
     FOREIGN KEY (zipcode) REFERENCES addres (zipcode)
 );
 
+INSERT INTO employee (
+    eid,
+    name,
+    email,
+    cellphone,
+    document,
+    birth,
+    zipcode,
+    housenumber,
+    complement,
+    photo
+) VALUES (
+    '1',
+    'Adolfo Lindofo',
+    'adolfo@lindofo.com',
+    '(21) 999987777',
+    '4789',
+    '1992-09-17',
+    '01020-345',
+    '1560',
+    'Travessa Lírios',
+    'https://randomuser.me/api/portraits/men/19.jpg'
+), (
+    '2',
+    'Wandinha Addams',
+    'wand@addams.com',
+    '(21) 987654321',
+    '1567',
+    '2002-10-31',
+    '12345-678',
+    '6667',
+    'fundos',
+    'https://randomuser.me/api/portraits/women/18.jpg'
+), (
+    '3',
+    'Diana Ianna',
+    'diana@realeza.com',
+    '(21) 223345566',
+    '2100',
+    '1989-04-22',
+    '12345-678',
+    '120',
+    'Sobrado',
+    'https://randomuser.me/api/portraits/women/8.jpg'
+);
+
 CREATE TABLE teacher (
     tid INT PRIMARY KEY AUTO_INCREMENT,
     availability VARCHAR (255) NOT NULL,
@@ -53,3 +154,88 @@ CREATE TABLE teacher (
 
     FOREIGN KEY (eid) REFERENCES employee (eid)
 );
+
+INSERT INTO teacher (
+    tid,
+    availability,
+    eid
+) VALUES (
+    '1',
+    'segunda e sexta o dia todo',
+    '3'
+), (
+    '2',
+    'terca, quarta e quinta somente a tarde',
+    '1'
+);
+
+CREATE TABLE activity (
+    aid INT PRIMARY KEY AUTO_INCREMENT,
+    nameactivity VARCHAR (255) NOT NULL,
+    description VARCHAR (255) NOT NULL
+);
+
+INSERT INTO activity (
+    aid,
+    nameactivity,
+    description
+) VALUES (
+    '1',
+    'Jumpp',
+    'atividade realizada pulando em um trampolim'
+), (
+    '2',
+    'Spinning',
+    'atividade realizada em uma bicicleta com subidas'
+);
+
+CREATE TABLE enableteacher (
+    etid INT PRIMARY KEY AUTO_INCREMENT,
+    aid INTEGER NOT NULL,
+    tid INTEGER NOT NULL,
+
+    FOREIGN KEY (aid) REFERENCES activity (aid),
+    FOREIGN KEY (tid) REFERENCES teacher (tid)
+);
+
+CREATE TABLE class (
+    cid INT PRIMARY KEY AUTO_INCREMENT,
+    dateclass DATE NOT NULL,
+    hour VARCHAR (255) NOT NULL,
+    tid INTEGER NOT NULL,
+    aid INTEGER NOT NULL,
+
+    FOREIGN KEY (tid) REFERENCES teacher (tid),
+    FOREIGN KEY (aid) REFERENCES activity (aid)
+);
+
+CREATE TABLE studentclass (
+    tlclass INTEGER PRIMARY KEY AUTO_INCREMENT,
+    sid INT NOT NULL,
+    aid INT NOT NULL,
+
+    FOREIGN KEY (sid) REFERENCES student (sid),
+    FOREIGN KEY (aid) REFERENCES activity (aid)
+);
+
+CREATE TABLE product (
+    productcode INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR (255) NOT NULL,
+    color VARCHAR (255) NOT NULL,
+    cost DOUBLE NOT NULL,
+    size CHAR(2) NOT NULL,
+    amount INTEGER NOT NULL
+);
+
+CREATE TABLE sale (
+    sid INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE NOT NULL,
+    cost DOUBLE NOT NULL,
+    amount INTEGER NOT NULL,
+    productcode INTEGER NOT NULL,
+    eid INTEGER NOT NULL,
+
+    FOREIGN KEY (productcode) REFERENCES product (productcode),
+    FOREIGN KEY (eid) REFERENCES employee (eid)
+);
+
